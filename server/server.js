6f14be7cd7b43fa7f36b8bb2750f312f5c6f1065
Jack Stein <jacklsteiniv@gmail.com>
2016-02-13 12:07:39 -0800

    Node back-end server + API test endpoint up and running.

--- server/server.js
+++ server/server.js
@@ -7,8 +7,7 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var Promise  = require('request-promise');
-var port = process.env.PORT || 8000;
-console.log("Listening on port" + port);
+var port = 8080;
 
 //app config
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,15 +17,32 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \
-Authorization');
-next();
+  Authorization');
+  next();
 });
 
 //log to console
 
-app.use(morgan);
+app.use(morgan("combined"));
 
 //set up your routes for API.
 //eventually move these into config folder, routes file.
+
+app.get('/', function(req, res) {
+  res.json({message: "Welcome"})
+});
+
+var apiRouter = express.Router();
+
+apiRouter.get('/', function(req, res) {
+  res.json({message: "Welcome to the User API for Rent control!"})
+})
+
+app.use('/api', apiRouter);
+
+//listen on port 8080
+
+app.listen(port);
+console.log("Listening on port " + port);
